e92df9cdd156feecfa2755b1076050c0a99829ed
Asadbek <huntsman6004@gmail.com>
2024-05-26 00:14:20 +0500

    update: production

--- core/settings/production.py
+++ core/settings/production.py
@@ -10,16 +10,14 @@ DEBUG = True
 # Django security
 ###################################################################
 
-USE_X_FORWARDED_HOST = True
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# USE_X_FORWARDED_HOST = True
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost",
     "http://127.0.0.1",
-    "http://127.0.0.1:8000",
-    "http://kindergarten-ms.techcraft.uz",
-    "https://kindergarten-ms.techcraft.uz",
+    "http://0.0.0.0",
 ]
 
 ###################################################################
