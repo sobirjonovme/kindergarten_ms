b1225716e99444694eccd9a099884b9f9c382570
Asadbek <huntsman6004@gmail.com>
2024-05-27 23:35:05 +0500

    update: production settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -13,9 +13,12 @@ DEBUG = True
 # USE_X_FORWARDED_HOST = True
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
+ENV_TRUSTED_ORIGINS = env.list("TRUSTED_ORIGINS", default=[])  # noqa
+
 CSRF_COOKIE_SECURE = False
 CSRF_TRUSTED_ORIGINS = [
     "http://*",
+    *ENV_TRUSTED_ORIGINS,
 ]
 
 ###################################################################
