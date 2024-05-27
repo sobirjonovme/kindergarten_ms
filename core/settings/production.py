2982d8321e111bc788803ac0780f3bd3b50ff809
Asadbek <huntsman6004@gmail.com>
2024-05-27 22:49:30 +0500

    update: production

--- core/settings/production.py
+++ core/settings/production.py
@@ -13,12 +13,10 @@ DEBUG = True
 # USE_X_FORWARDED_HOST = True
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-CSRF_COOKIE_SECURE = True
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://localhost",
-#     "http://127.0.0.1",
-#     "http://0.0.0.0",
-# ]
+CSRF_COOKIE_SECURE = False
+CSRF_TRUSTED_ORIGINS = [
+    "http://*",
+]
 
 ###################################################################
 # CORS
